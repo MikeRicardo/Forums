3883f01e34716c3d699e400d642daa85a48e521a
MikeRicardo <m.michael.ricardo@gmail.com>
2012-10-09 21:18:15 -0400

    first commit

---
+++ Connect_Data.php
@@ -0,0 +1,6 @@
+<?php
+$server		= 'localhost';
+$password	= '';
+$username	= 'root';
+$database	= 'forum';
+?>
---
+++ footer.php
@@ -0,0 +1,5 @@
+	</div><!-- content -->
+</div><!-- wrapper -->
+<div id="footer">Made in 2010</div>
+</body>
+</html>
